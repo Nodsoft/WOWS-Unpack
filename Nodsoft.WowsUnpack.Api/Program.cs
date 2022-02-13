b0e7228000035a0d4ebc9f128e18388ac17f1dce
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-02-13 15:01:34 +0100

    Implement Serilog w/ logger config

--- Nodsoft.WowsUnpack.Api/Program.cs
+++ Nodsoft.WowsUnpack.Api/Program.cs
@@ -1,3 +1,6 @@
+using Serilog;
+using Serilog.Events;
+
 namespace Nodsoft.WowsUnpack.Api;
 
 public static class Program
@@ -5,25 +8,30 @@ public static class Program
 	public static async Task Main(string[] args)
 	{
 		using IHost host = CreateHostBuilder(args).Build();
+		IConfiguration configuration = host.Services.GetRequiredService<IConfiguration>();
 		
+		Log.Logger = new LoggerConfiguration()
+			.ReadFrom.Configuration(configuration)
+			.CreateLogger();
+
 		await host.RunAsync();
 	}
 	
 	
 	public static IHostBuilder CreateHostBuilder(string[] args) =>
 		Host.CreateDefaultBuilder(args)
+			.UseSerilog()
 			.ConfigureWebHostDefaults(webBuilder =>
 			{
 				webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
 				{
 					config.SetBasePath(Directory.GetCurrentDirectory());
-					config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-						.AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+					config.AddJsonFile("appsettings.json", true, true)
+						.AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
 					config.AddEnvironmentVariables();
 					config.AddCommandLine(args);
 				});
 
 				webBuilder.UseStartup<Startup>();
-				//webBuilder.UseSerilog(); 
 			});
 }
