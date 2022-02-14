deb3c6ae0ab6c42dfc912e4ed1662b2ee170e627
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-02-14 12:49:01 +0100

    Draft Python runner implementation

--- Nodsoft.WowsUnpack.Api/Controllers/ReplayController.cs
+++ Nodsoft.WowsUnpack.Api/Controllers/ReplayController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Nodsoft.WowsUnpack.Api.Services;
 
 namespace Nodsoft.WowsUnpack.Api.Controllers;
 
@@ -6,10 +7,22 @@ namespace Nodsoft.WowsUnpack.Api.Controllers;
 [ApiVersion("1.0")]
 public class ReplayController : ControllerBase
 {
+	private readonly PythonRunner _runner;
+
+	public ReplayController(PythonRunner runner)
+	{
+		_runner = runner;
+	}
+	
 	
 	[HttpGet]
-	public void Index()
+	public async Task<string> Index()
 	{
-		
+		const string cmd = @".\replay_parser.py --replay .\0.11.0.wowsreplay";
+		//const string cmd = @"python .\replay_parser.py --replay .\0.11.0.wowsreplay";
+		//const string cmd = @"echo $env:PATH";
+		//const string cmd = @"pwd";
+
+		return await _runner.RunScriptAsync(cmd);
 	}
 }
---
+++ Nodsoft.WowsUnpack.Api/Services/PythonRunner.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Nodsoft.WowsUnpack.Api.Services;
+
+/// <summary>
+/// POC class to experiment with Python processes
+/// </summary>
+public class PythonRunner
+{
+	private readonly ILogger<PythonRunner> _logger;
+
+	public PythonRunner(ILogger<PythonRunner> logger)
+	{
+		_logger = logger;
+	}
+
+	public async Task<string> RunScriptAsync(string args)
+	{
+		/*
+		 * TODO: Get OS-specific shell executable
+		 */
+		string shellExec = string.Empty;
+
+		//shellExec = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? @"C:\Windows\System32\WindowsPowershell\v1.0\powershell.exe" : @"/bin/bash";
+		shellExec = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? @"C:\Python310\python.exe" : throw new NotImplementedException();
+
+//		if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+//		{
+//			args = "/C " + args;
+//		}
+		
+		ProcessStartInfo startInfo = new()
+		{
+			UseShellExecute = false,
+			RedirectStandardOutput = true,
+			FileName = shellExec,
+			Arguments = args,
+			WorkingDirectory = @"D:\Code\Source\WOWS-Unpack\replays_unpack"
+		};
+		
+		using Process? process = Process.Start(startInfo);
+
+		if (process is not null)
+		{
+			using StreamReader reader = process.StandardOutput;
+			return await reader.ReadToEndAsync();
+		}
+
+		return string.Empty;
+	}
+}
