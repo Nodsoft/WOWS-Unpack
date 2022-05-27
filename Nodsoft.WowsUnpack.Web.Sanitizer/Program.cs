8abb7bdf7488384b2d17b2d03b242fd893e85a86
Sakura Akeno Isayeki <sakura.isayeki@nodsoft.net>
2022-05-27 17:01:44 +0200

    Add basic dependencies
    Used some visual assets from the SocialGuard WASM projects

--- Nodsoft.WowsUnpack.Web.Sanitizer/Program.cs
+++ Nodsoft.WowsUnpack.Web.Sanitizer/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Nodsoft.WowsReplaysUnpack;
 using Nodsoft.WowsUnpack.Web.Sanitizer;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<ReplayUnpacker>();
 
 await builder.Build().RunAsync();
